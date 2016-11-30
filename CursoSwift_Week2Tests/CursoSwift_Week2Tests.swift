7df81bf4e807c9039b91664d417c5e0db44e1f9f
Jose Garcia <josegarcia@Joses-Mac.local>
2016-11-30 06:59:00 -0800

    Initial Commit

---
+++ CursoSwift_Week2Tests/CursoSwift_Week2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  CursoSwift_Week2Tests.swift
+//  CursoSwift_Week2Tests
+//
+//  Created by Jose Garcia on 30/11/16.
+//  Copyright © 2016 Jose Garcia. All rights reserved.
+//
+
+import XCTest
+@testable import CursoSwift_Week2
+
+class CursoSwift_Week2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
